335fec063463d74d9f5547bc57aa8df90a29445a
DiegoGuzman <guzman.tapayuri.diego>
2024-05-23 17:16:45 -0500

    primer comit

---
+++ scrip.js
@@ -0,0 +1,37 @@
+let menuVisible = false;
+//funcion para mostrar y ocultar el menu//
+function mostrarOcultarMenu(){
+    if (menuVisible) {
+        document.getElementById("nav").classList = "";
+        menuVisible = false; 
+    } else {
+        document.getElementById("nav").classList = "responsive";
+        menuVisible = true;
+    }
+}
+
+function seleccionar(){
+    //oculto el menu una vez que selecciono una opcion
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+}
+//Funcion que aplica las animaciones de las habilidades
+function efectoHabilidades(){
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills >= 300){
+        let habilidades = document.getElementsByClassName("progreso");
+        habilidades[0].classList.add("javascript");
+        habilidades[1].classList.add("htmlcss");
+        habilidades[2].classList.add("photoshop");
+        habilidades[3].classList.add("comunicacion");
+        habilidades[4].classList.add("trabajo");
+        habilidades[5].classList.add("creatividad");
+    }
+}
+
+
+//detecto el scrolling para aplicar la animacion de la barra de habilidades
+window.onscroll = function(){
+    efectoHabilidades();
+} 
